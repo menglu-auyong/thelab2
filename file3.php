bfc1d8be6988c9afeae6c3a30fd420faaf8aedbe
MengLu Au Yong <menglu@woodwing.com>
2018-10-11 17:44:17 +0200

    Commiting from subtree/relesae/10.1.x Cherry-picking from another machine
    (cherry picked from commit c1f79fb1605f8bbcb176dcceb0f246dcd62eb953)
    Signed-off-by: MengLu Au Yong <menglu@woodwing.com>

--- file3.php
+++ file3.php
@@ -5,3 +5,4 @@ echo 'file3";
 ## Adjusted from the lab2 subtree repo subtree/release/10.1.x  
 ## Adjusted from the lab2 subtree repo subtree/relesae/10.1.x ( but this time also adjusting in Enterprise )
 ## Adjusted from the lab2 subtree repo subtree/relesae/10.1.x ( but this time also adjusting in Enterprise and another two commits only involving Ent files)
+## Adjusted from the lab2 subtree repo subtree/release/10.1.x ( trying out to apply this change from another machine Enterprise )
