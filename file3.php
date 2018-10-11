fefe7baeb07f6a04a0a665588ced5fedec0299f7
MengLu Au Yong <menglu@woodwing.com>
2018-10-11 15:33:38 +0200

    Commited files from subtree and Enterprise from experiment Merged from subtree 10.1.x ( but hopefully it will exclude the files/histories from Enterprise )
    (cherry picked from commit 0604005539647ec2de38e006dbae32077a196177)
    Signed-off-by: MengLu Au Yong <menglu@woodwing.com>

--- file3.php
+++ file3.php
@@ -3,3 +3,4 @@
 echo 'file3";
 ## Adjusted from the lab2 repo lab2/release/10.1.x.
 ## Adjusted from the lab2 subtree repo subtree/release/10.1.x  
+## Adjusted from the lab2 subtree repo subtree/relesae/10.1.x ( but this time also adjusting in Enterprise )
