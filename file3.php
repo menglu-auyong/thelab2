371be78a32b27d7b8b7125256df63ebde3d5d8b3
MengLu Au Yong <menglu@woodwing.com>
2018-10-11 16:31:15 +0200

    commiting files from subtree - subtree and enterprise - Part 2 Only cherry-picking the one that involves subtree folder ( but should exclude the fix from Enterprise )
    (cherry picked from commit ed32ca40fde68f07aa4122b7c310461b1546aa5d)
    Signed-off-by: MengLu Au Yong <menglu@woodwing.com>

--- file3.php
+++ file3.php
@@ -4,3 +4,4 @@ echo 'file3";
 ## Adjusted from the lab2 repo lab2/release/10.1.x.
 ## Adjusted from the lab2 subtree repo subtree/release/10.1.x  
 ## Adjusted from the lab2 subtree repo subtree/relesae/10.1.x ( but this time also adjusting in Enterprise )
+## Adjusted from the lab2 subtree repo subtree/relesae/10.1.x ( but this time also adjusting in Enterprise and another two commits only involving Ent files)
