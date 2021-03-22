f3720068674906b443547140cdfaf8c239799022
Edwin van der Klaauw <ekl@woodwing.com>
2021-03-22 15:12:28 +0100

    feature 2

--- file2.php
+++ file2.php
@@ -1,12 +1,3 @@
 <?php
 
-// TODO: check if this is indeed file2.php
-echo "file2.php";
-
-## Fixing thelab2/10.1.x in experiment/plugins/thelab2/file2.php (added as subtree) 
-
-echo "feature D";
-
-echo "work1";
-
-echo "feature E";
+echo "feature 2";
